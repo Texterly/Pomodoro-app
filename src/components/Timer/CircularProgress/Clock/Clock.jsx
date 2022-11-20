fd1c2c08c3b264c201f85d4eabbe6a554cc6761c
Texterly <88582978+Texterly@users.noreply.github.com>
2022-11-20 18:44:49 +0300

    modified Clock

--- src/components/Timer/CircularProgress/Clock/Clock.jsx
+++ src/components/Timer/CircularProgress/Clock/Clock.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 function Clock() {
+  const [time, setTime] = useState(100);
+  const [isActive, setIsActive] = useState(false);
+  useEffect(() => {
+    if (isActive && time > 0) {
+      const inteval = setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+      return () => clearInterval(inteval);
+    }
+  }, [time, isActive]);
+
+  const toggleClock = () => {
+    setIsActive(!isActive);
+  };
+
+  const getTime = (time) => {
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+    return `${min < 10 ? '0' + min : min} : ${sec < 10 ? '0' + sec : sec}`;
+  };
+
   return (
     <ClockContainer>
-      <TimerText>05:00</TimerText>
+      <TimerText>{getTime(time)}</TimerText>
+      <StartPauseButton onClick={toggleClock}>
+        {isActive ? 'Pause' : 'Start'}
+      </StartPauseButton>
     </ClockContainer>
   );
 }
@@ -12,9 +36,18 @@ function Clock() {
 export default Clock;
 
 const ClockContainer = styled.div`
-
+  display: grid;
+  place-items: center;
 `;
 
 const TimerText = styled.h3`
     font-size: 10rem;
+`;
+
+const StartPauseButton = styled.button`
+  all: unset;
+  text-align: center;
+  font-size: 6rem;
+  text-transform: uppercase;
+  letter-spacing: 1rem;
 `;
